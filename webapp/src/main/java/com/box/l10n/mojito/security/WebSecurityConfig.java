2880ec16d2d6861e2f5ce1247a3afb4f71c9d7bf
Jean Aurambault <ja@openai.com>
2026-05-21 11:53:22 -0700

    refactor(aspectj): remove security aspect mode

--- webapp/src/main/java/com/box/l10n/mojito/security/WebSecurityConfig.java
+++ webapp/src/main/java/com/box/l10n/mojito/security/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -18,7 +17,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.authentication.configurers.ldap.LdapAuthenticationProviderConfigurer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,8 +32,6 @@ import org.springframework.util.StringUtils;
  * @author wyau
  */
 @EnableWebSecurity
-// TOOD(spring2) we don't use method level security, do we? remove?
-@EnableGlobalMethodSecurity(securedEnabled = true, mode = AdviceMode.ASPECTJ)
 @Configuration
 @ConditionalOnAuthTypes(
     anyOf = {
