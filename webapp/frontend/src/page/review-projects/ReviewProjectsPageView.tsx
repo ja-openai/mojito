7c44dc1e58f7ea0534e97bc3ec4f5d762178dee0
Jean Aurambault <ja@openai.com>
2026-06-18 18:28:45 -0700

    fix(review-projects): use locale selection presets

--- webapp/frontend/src/page/review-projects/ReviewProjectsPageView.tsx
+++ webapp/frontend/src/page/review-projects/ReviewProjectsPageView.tsx
@@ -1213,6 +1213,7 @@ function FilterControls({
         onChange={filters.onLocaleChange}
         buttonAriaLabel="Filter by locale"
         myLocaleTags={filters.myLocaleTags}
+        showSelectionPresets
       />
       <SearchControl
         value={filters.searchQuery}
