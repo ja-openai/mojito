ae468ecb649460469cc6d1be2ccabe9fbe12c83b
Jean Aurambault <ja@openai.com>
2026-09-22 08:04:29 -0700

    refactor(cli): replace double braces without regex

--- cli/src/main/java/com/box/l10n/mojito/cli/command/checks/DoubleBracesPlaceholderDescriptionChecker.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/checks/DoubleBracesPlaceholderDescriptionChecker.java
@@ -5,15 +5,12 @@ import java.util.Set;
 public class DoubleBracesPlaceholderDescriptionChecker
     extends SingleBracesPlaceholderDescriptionChecker {
 
-  private static final String LEFT_DOUBLE_BRACES_REGEX = "\\{\\{.*?";
-  private static final String RIGHT_DOUBLE_BRACES_REGEX = "\\}\\}.*?";
-
   @Override
   public Set<String> checkCommentForDescriptions(String source, String comment) {
     return super.checkCommentForDescriptions(replaceDoubleBracesWithSingle(source), comment);
   }
 
   private String replaceDoubleBracesWithSingle(String str) {
-    return str.replaceAll(LEFT_DOUBLE_BRACES_REGEX, "{").replaceAll(RIGHT_DOUBLE_BRACES_REGEX, "}");
+    return str.replace("{{", "{").replace("}}", "}");
   }
 }
